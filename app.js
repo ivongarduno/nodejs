c3bdf86ff78c82f157771a8d40bf3053916dae15
ivongarduno <ivonne_1412@hotmail.com>
2021-05-06 10:25:57 -0500

    require whithout export

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // CommonJS, every file is module (by default)
 // Modules - Encapsulated Code (only share minimum)
-const names = require('./4-names')
-const sayHi = require('./5-utils')
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavor");
+require("./7-without-export")
 
-sayHi('susan')
-sayHi(names.john)
-sayHi(names.peter)
+
+console.log(data);
+sayHi("susan");
+sayHi(names.john);
+sayHi(names.peter);
